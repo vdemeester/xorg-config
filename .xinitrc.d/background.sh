2b2b544ecec7ee5af50f4508e59cf2edcfdae460
Vincent Demeester <vincent@demeester.fr>
2011-12-25 12:07:47 +0100

    Update background.sh

--- .xinitrc.d/background.sh
+++ .xinitrc.d/background.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # Set the background (ugly right now, isn't it ?)
 # TODO: some "at login" random wallpaper display.
-fbsetroot -gradient Crossdiagonal -from "#eeeeee" -to "#666666"
+FEH="$(which feh)"
+FBSETROOT="$(which fbsetroot)"
+if test -x "$FEH" && test -e $HOME/pictures/wallpaper; then
+    $FEH --bg-fill $HOME/pictures/wallpaper
+else
+    if test -x "$FBSETROOT"; then
+        $FBSETROOT -gradient Crossdiagonal -from "#eeeeee" -to "#666666"
+    fi
+fi
