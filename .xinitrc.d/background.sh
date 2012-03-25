7e12d027c8091a001434f7e3239b9a5d485549b2
Vincent Demeester <vincent+git@demeester.fr>
2012-03-25 18:42:28 +0200

    Update background.sh to auto detect pictures folder

--- .xinitrc.d/background.sh
+++ .xinitrc.d/background.sh
@@ -3,8 +3,14 @@
 # TODO: some "at login" random wallpaper display.
 FEH="$(which feh)"
 FBSETROOT="$(which fbsetroot)"
-if test -x "$FEH" && test -e $HOME/pictures/wallpaper; then
-    $FEH --bg-fill $HOME/pictures/wallpaper
+# Get pictures folder from xdg-user-dir if available
+command -v xdg-user-dir >/dev/null && {
+    PICTURES="$(xdg-user-dir PICTURES)"
+}
+# Set a default value as $HOME/pictures/
+test -z "${PICTURES}" && PICTURES="$HOME/pictures"
+if test -x "$FEH" && test -e $PICTURES/wallpaper; then
+    $FEH --bg-fill $PICTURES/wallpaper
 else
     if test -x "$FBSETROOT"; then
         $FBSETROOT -gradient Crossdiagonal -from "#eeeeee" -to "#666666"
