ded869d20d684fe297ecb876740a370464cd5ac1
Vincent Demeester <vincent+git@demeester.fr>
2012-05-05 16:42:33 +0200

    Start window manager early on

---
+++ .xinitrc.d/post.unclutter.sh
@@ -0,0 +1,9 @@
+# --------------------------------------
+# file:			~/.xinitrc.d/post.unclutter.sh
+# author:		Vincent Demeester
+# vim:nu:ai:si:et:ts=4:sw=4:ft=sh
+# --------------------------------------
+
+# hide curson
+unclutter -idle 5 &
+
---
+++ .xinitrc.d/pre.xrdb.sh
@@ -0,0 +1,9 @@
+# --------------------------------------
+# file:			~/.xinitrc.d/pre.xrdb.sh
+# author:		Vincent Demeester
+# vim:nu:ai:si:et:ts=4:sw=4:ft=sh
+# --------------------------------------
+
+# Load .Xdefaults
+xrdb -merge ~/.Xdefaults
+
