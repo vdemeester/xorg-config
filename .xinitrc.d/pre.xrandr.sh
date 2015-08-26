1a3977d1ddd5bbbee5c693ba7d1d016be0b68c31
Vincent Demeester <vincent@sbr.pm>
2015-08-26 08:32:26 +0200

    Remove set-e from .xinitrc.d scripts

--- .xinitrc.d/pre.xrandr.sh
+++ .xinitrc.d/pre.xrandr.sh
@@ -1,6 +1,5 @@
 # Run xrandr with specific stuff on machine(s)
 # Let's do the hard way for now
-set -e
 
 test $(hostname) = "honshu" && {
     xrandr --output eDP1 --mode 1600x900 --output VGA1 --auto --right-of eDP1
