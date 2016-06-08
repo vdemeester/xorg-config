798cd22a22af3a36bdcebaecf0323b3d49b5ffea
Vincent Demeester <vincent@sbr.pm>
2016-06-08 20:25:11 +0200

    Update some xorg configurations
    - Add xmodmap + startup script
    - Update XCompose with some fun ♥
    - Update Xdefault to add Symbola for emoji support in terminal
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ .xinitrc.d/post.xmodmap.sh
@@ -0,0 +1,6 @@
+# ------------------------------------------
+# file:     ~/.xinitrc.d/post.xmodemap.sh
+# author:   Vincent Demeester
+# vim:nu:ai:si:et:ts=4:sw=4:ft=sh
+# ------------------------------------------
+xmodmap ~/.Xmodmap
